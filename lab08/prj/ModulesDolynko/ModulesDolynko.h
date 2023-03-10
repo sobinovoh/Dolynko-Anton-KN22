ab3c035c50cc2af61575deb3d269deb5a452d824
adolynko <dolynkoanton@gmail.com>
2023-03-10 11:52:44 +0200

    added lib

---
+++ lab08/prj/ModulesDolynko/ModulesDolynko.h
@@ -0,0 +1,14 @@
+#ifndef MODULESDOLYNKO_H_INCLUDED
+#define MODULESDOLYNKO_H_INCLUDED
+
+
+
+#endif // MODULESDOLYNKO_H_INCLUDED
+
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+float s_calculation(int x, int y, int z);
